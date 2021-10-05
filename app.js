2476492b93bc15bc9ea706785562bec330ddbbf3
fora-heims <foraheims@gmail.com>
2021-10-04 19:04:28 -0700

    made text input box and submit button dissapear once the game has been won or lost. Also added enter button feature, for submiting a guess as well as reseting the game.

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ guess.addEventListener('click', ()=> {
         wins.textContent = newWins;
         replay.style.display = 'block';
         table.style.display = 'table';
+        input.style.display = 'none';
+        guess.style.display = 'none';
     } else if (attempt >= 4) {
         let message = 'Out of attempts. You Lose. Try again.';
         results.textContent = `${message}`;
@@ -36,6 +38,8 @@ guess.addEventListener('click', ()=> {
         losses.textContent = newLosses;
         replay.style.display = 'block';
         table.style.display = 'table';
+        input.style.display = 'none';
+        guess.style.display = 'none';
     } else if (num > rndNum) {
         let message = `Too high. You have ${4 - attempt} attempts left.`;
         results.textContent = `${message}`;
@@ -43,7 +47,52 @@ guess.addEventListener('click', ()=> {
         let message = `Too low. You have ${4 - attempt} attempts left.`;
         results.textContent = `${message}`;
     }
-    // update DOM to reflect the new state
+});
+
+window.addEventListener('keyup', event => {
+    if (event.key === 'Enter') {
+        if (input.style.display === 'block') {
+            attempt++;
+            results.style.display = 'inline';
+            let num = Number(input.value);
+            if (num > 20 || isNaN(num) === 'true') {
+                let message = `Input invalid. You have ${4 - attempt} attempts left.`;
+                results.textContent = `${message}`;
+            } else if (num === rndNum) {
+                let message = 'You Win!';
+                results.textContent = `${message}`;
+                let newWins = Number(wins.textContent) + 1;
+                wins.textContent = newWins;
+                replay.style.display = 'block';
+                table.style.display = 'table';
+                input.style.display = 'none';
+                guess.style.display = 'none';
+            } else if (attempt >= 4) {
+                let message = 'Out of attempts. You Lose. Try again.';
+                results.textContent = `${message}`;
+                let newLosses = Number(losses.textContent) + 1;
+                losses.textContent = newLosses;
+                replay.style.display = 'block';
+                table.style.display = 'table';
+                input.style.display = 'none';
+                guess.style.display = 'none';
+            } else if (num > rndNum) {
+                let message = `Too high. You have ${4 - attempt} attempts left.`;
+                results.textContent = `${message}`;
+            } else if (num < rndNum) {
+                let message = `Too low. You have ${4 - attempt} attempts left.`;
+                results.textContent = `${message}`;
+            }} else {
+            attempt = 0;
+            rndNum = Math.floor(Math.random() * 20) + 1;
+            results.textContent = `You have ${4 - attempt} attempts.`;
+            replay.style.display = 'none';
+            table.style.display = 'none';
+            input.value = '';
+            input.style.display = 'block';
+            guess.style.display = 'block';
+        }
+    }
 });
 
 replay.addEventListener('click', () => {
@@ -53,6 +102,8 @@ replay.addEventListener('click', () => {
     replay.style.display = 'none';
     table.style.display = 'none';
     input.value = '';
+    input.style.display = 'block';
+    guess.style.display = 'block';
 });
 
 score.addEventListener('click', () => {
