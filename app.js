04b1686a0bc0b2fdab6cd242f139587cba7659bc
fora-heims <foraheims@gmail.com>
2021-10-04 18:49:23 -0700

    display/hide score (wins/losses) button created and functional. Slight style adjusments

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const results = document.getElementById('guess-results');
 const table = document.getElementById('wins-losses');
 let wins = document.getElementById('wins');
 let losses = document.getElementById('losses');
+const score = document.getElementById('score-button');
 
 // initialize global state
 let rndNum = Math.floor(Math.random() * 20) + 1;
@@ -16,9 +17,7 @@ console.log(wins.textContent);
 // Add event listeners
 guess.addEventListener('click', ()=> {
     attempt++;
-    console.log(attempt)
     results.style.display = 'inline';
-    table.style.display = 'table';
     let num = Number(input.value);
     if (num > 20 || isNaN(num) === 'true') {
         let message = `Input invalid. You have ${4 - attempt} attempts left.`;
@@ -29,12 +28,14 @@ guess.addEventListener('click', ()=> {
         let newWins = Number(wins.textContent) + 1;
         wins.textContent = newWins;
         replay.style.display = 'block';
+        table.style.display = 'table';
     } else if (attempt >= 4) {
         let message = 'Out of attempts. You Lose. Try again.';
         results.textContent = `${message}`;
         let newLosses = Number(losses.textContent) + 1;
         losses.textContent = newLosses;
         replay.style.display = 'block';
+        table.style.display = 'table';
     } else if (num > rndNum) {
         let message = `Too high. You have ${4 - attempt} attempts left.`;
         results.textContent = `${message}`;
@@ -51,4 +52,15 @@ replay.addEventListener('click', () => {
     results.textContent = `You have ${4 - attempt} attempts.`;
     replay.style.display = 'none';
     table.style.display = 'none';
+    input.value = '';
+});
+
+score.addEventListener('click', () => {
+    if (table.style.display === 'none') {
+        table.style.display = 'table';
+        score.textContent = 'Hide Score';
+    } else {
+        table.style.display = 'none';
+        score.textContent = 'Display Score';
+    }
 });
