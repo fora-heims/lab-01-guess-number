b1f9f16db0fb6535e8a32fb8f54501da8f2502ae
fora-heims <foraheims@gmail.com>
2021-10-04 17:41:49 -0700

    Else if statement works to display message. Allows 4 attempts to guess number. message to let know if too high or low works.

--- app.js
+++ app.js
@@ -2,16 +2,16 @@
 let input = document.getElementById('user-input');
 const guess = document.getElementById('guess-button');
 const replay = document.getElementById('reset-button');
-let results = document.getElementById('guess-results');
+const results = document.getElementById('guess-results');
 const table = document.getElementById('wins-losses');
-const wins = document.getElementById('wins');
-const losses = document.getElementById('losses');
+let wins = document.getElementById('wins');
+let losses = document.getElementById('losses');
 // console.log(input, guess, replay, results, wins, losses);
 
 // initialize global state
-let rndNum = Math.floor(Math.random() * 20) +1;
+let rndNum = Math.floor(Math.random() * 20) + 1;
 let attempt = 0;
-console.log(rndNum, attempt);
+// console.log(rndNum, attempt);
 
 // set event listeners
 guess.addEventListener('click', ()=> {
@@ -19,22 +19,28 @@ guess.addEventListener('click', ()=> {
     // use user input to update state 
     attempt++;
     // console.log(attempt);
-    results.style.display = 'vis';
-    table.style.display = 'vis';
-    replay.style.display = 'vis';
+    results.style.display = 'inline';
+    console.log(results.style.display);
+    table.style.display = 'table';
     let num = Number(input.value);
     if (num === rndNum) {
         let message = 'You Win!';
-        results.textContent = message;
+        results.textContent = `${message}`;
+        let newWins = Number(wins) + 1;
+        wins.textContent = newWins;
     } else if (attempt >= 4) {
         let message = 'Out of attempts. You Lose. Try again.';
-        results.textContent = message;
+        results.textContent = `${message}`;
+        let newLosses = Number(losses) + 1;
+        wins.textContent = newLosses;
+        replay.style.display = 'block';
     } else if (num > rndNum) {
-        let message = 'Too high.';
-        results.textContent = message;
+        let message = `Too high. You have ${4 - attempt} attempts left.`;
+        results.textContent = `${message}`;
     } else if (num < rndNum) {
-        let message = 'Too low.';
-        results.textContent = message;
+        let message = `Too low. You have ${4 - attempt} attempts left.`;
+        results.textContent = `${message}`;
     }
+
     // update DOM to reflect the new state
 });
