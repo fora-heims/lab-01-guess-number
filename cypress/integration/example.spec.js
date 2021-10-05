690de1f7c016b1f6469c6b2ac8bbd30a635aae4d
fora-heims <foraheims@gmail.com>
2021-10-05 11:08:16 -0700

    First cypress test made and passing. Second test made and passing, though currently the button requires two clicks to work properly, so this verified and helped define a small bug I had suspicions about previously.

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -1,12 +1,20 @@
 
-// describe('Example Test', ()=>{
-//     before(()=>{
-//         cy.visit('/');
-//     });
+describe('Example Test', ()=>{
+    before(()=>{
+        cy.visit('/');
+    });
 
-//     describe('HTML elements', ()=>{   
-//         it('contains the alchemy logo', ()=>{
-//             cy.get('.main-logo').should('be.visible');
-//         });
-//     });
-// });
+    describe('HTML elements', ()=>{   
+        it('conatains hidden score table', ()=>{
+            cy.get('#wins-losses').should('be.hidden');
+        });
+    });
+
+    describe('user interactions', ()=>{   
+        it('score table displayed when Display Score button is clicked', ()=>{
+            cy.get('#score-button').click();
+            cy.get('#score-button').click();
+            cy.get('#wins-losses').should('be.visible');
+        });
+    });
+});
