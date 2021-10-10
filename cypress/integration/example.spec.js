04b405f69c2663e31bc2c86284f3623e3718689c
fora-heims <foraheims@gmail.com>
2021-10-10 14:15:16 -0700

    Cypress tests passing. Used .should('contain', '0') to access textContent in the cypress assertation.

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -6,14 +6,14 @@ describe('Example Test', ()=>{
 
     describe('HTML elements', ()=>{   
         it('conatains score table', ()=>{
-            cy.get('#wins-losses').should('be.table');
+            cy.get('#wins-losses').should('be.visible');
         });
     });
 
     describe('user interactions', ()=>{   
         it('wins set to 0 when Reset Score button is clicked', ()=>{
             cy.get('#reset-timer').click();
-            cy.get('#wins').should('be.0');
+            cy.get('#wins').should('contain', '0');
         });
     });
 });
